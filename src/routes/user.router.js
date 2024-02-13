15d62d04c7ae57fc7e30f93d1f2b6fbdc168b337
Jonathan David <jhernadezcorrea@gmail.com>
2024-02-13 14:39:15 -0500

    first commit

---
+++ src/routes/user.router.js
@@ -0,0 +1,31 @@
+/* const { router } = require('../app'); */
+const { getAll, create, getOne, remove, update, verifyUser, login, logged, reset_password, updatePassword } = require('../controllers/user.controllers');
+const express = require('express');
+const verifyJWT = require('../utils/verifyJWT');
+
+const routerUser = express.Router();
+
+routerUser.route('/')
+  .get(verifyJWT, getAll)
+  .post(create);
+
+routerUser.route('/login')
+  .post(login)
+
+routerUser.route('/me')
+  .get(verifyJWT, logged)
+
+routerUser.route("/reset_password")
+  .post(reset_password)
+routerUser.route("/verify/:code")
+  .get(verifyUser)
+
+routerUser.route("/reset_password/:code")
+  .post(updatePassword)
+
+routerUser.route('/:id')
+  .get(verifyJWT, getOne)
+  .delete(verifyJWT, remove)
+  .put(verifyJWT, update);
+
+module.exports = routerUser;
